268e93c210fe84e6afaebe384f9754149d4470ba
Vinicius Cosmi <vinicius.cosmi@gmail.com>
2017-07-10 10:25:18 -0300

    fix/bdd

--- features/cep.feature
+++ features/cep.feature
@@ -8,41 +8,41 @@ Funcionalidade: Cep
 @cep_valido
 Cenario: Verificar cep valido
     Dado que realizo uma consulta na api de cep
-    Quando informo um cep valido
-    Então deve me retornar os dados do endereco
+    Quando informar um cep valido
+    Então devo visualizar os dados do endereço
 
 @cep_inexistente
 Cenario: Verificar cep valido
     Dado que realizo uma consulta na api de cep
-    Quando informo um cep inexistente
-    Então deve me retornar uma mensagem de erro
+    Quando informar um cep inexistente
+    Então devo visualizar o erro tratado
 
 @cep_letras
 Cenario: Verificar cep com caracteres alfabéticos
     Dado que realizo uma consulta na api de cep
-    Quando informo um cep com caracteres alfabéticos
-    Então deve me retornar erro
+    Quando informar um cep com caracteres alfabéticos
+    Então devo visualizar o erro
 
 @cep_menor
 Cenario: Verificar cep com menos caracteres
     Dado que realizo uma consulta na api de cep
-    Quando informo um cep com menos caracteres
-    Então deve me retornar erro
+    Quando informar um cep com menos caracteres
+    Então devo visualizar o erro
 
 @cep_maior
 Cenario: Verificar cep com mais caracteres
     Dado que realizo uma consulta na api de cep
-    Quando informo um cep com mais caracteres
-    Então deve me retornar erro
+    Quando informar um cep com mais caracteres
+    Então devo visualizar o erro
 
 @cep_caracter_especial
 Cenario: Verificar cep com caracteres especiais
     Dado que realizo uma consulta na api de cep
-    Quando informo um cep com caracteres especiais
-    Então deve me retornar erro
+    Quando informar um cep com caracteres especiais
+    Então devo visualizar o erro
 
 @cep_branco
 Cenario: Verificar cep com caracteres especiais
     Dado que realizo uma consulta na api de cep
-    Quando informo um cep em branco
-    Então deve me retornar erro
+    Quando informar um cep em branco
+    Então devo visualizar o erro
--- features/step_definitions/cep.rb
+++ features/step_definitions/cep.rb
@@ -2,54 +2,55 @@ Dado(/^que realizo uma consulta na api de cep$/) do
   @uri = "http://viacep.com.br/ws/"
 end
 
-Quando(/^informo um cep valido$/) do
+Quando(/^informar um cep valido$/) do
   @url = @uri + CEP["cep"]["valido"]
   @response = HTTParty.get(@url)
 end
 
-Quando(/^informo um cep com caracteres alfabéticos$/) do
+Quando(/^informar um cep com caracteres alfabéticos$/) do
   @url = @uri + CEP["cep"]["letras"]
   @response = HTTParty.get(@url)
 end
 
-Quando(/^informo um cep com menos caracteres$/) do
+Quando(/^informar um cep com menos caracteres$/) do
   @url = @uri + CEP["cep"]["menos_numeros"]
   @response = HTTParty.get(@url)
 end
 
-Quando(/^informo um cep com mais caracteres$/) do
+Quando(/^informar um cep com mais caracteres$/) do
   @url = @uri + CEP["cep"]["mais_numeros"]
   @response = HTTParty.get(@url)
 end
 
-Quando(/^informo um cep com caracteres especiais$/) do
+Quando(/^informar um cep com caracteres especiais$/) do
   @url = @uri + CEP["cep"]["caracter_especial"]
   @response = HTTParty.get(@url)
 end
 
-Quando(/^informo um cep em branco$/) do
+Quando(/^informar um cep em branco$/) do
   @url = @uri + CEP["cep"]["branco"]
   @response = HTTParty.get(@url)
 end
 
-Quando(/^informo um cep inexistente$/) do
+Quando(/^informar um cep inexistente$/) do
   @url = @uri + CEP["cep"]["inexistente"]
   @response = HTTParty.get(@url)
 end
 
-Então(/^deve me retornar os dados do endereco$/) do
+Então(/^devo visualizar os dados do endereço$/) do
   puts @response.body
   CEP["cep"]["campos"].each{|cep| expect(@response.body).to include(cep)}
+  CEP["cep"]["resultado"].each{|cep| expect(@response.body.force_encoding 'UTF-8').to include(cep)}
   expect(@response.code).to eq 200
 end
 
-Então(/^deve me retornar erro$/) do
+Então(/^devo visualizar o erro$/) do
   puts @response.body
   CEP["cep"]["campos"].each{|cep| expect(@response.body).to_not include(cep)}
   expect(@response.code).to eq 400
 end
 
-Então(/^deve me retornar uma mensagem de erro$/) do
+Então(/^devo visualizar o erro tratado$/) do
   puts @response.body
   CEP["cep"]["campos"].each{|cep| expect(@response.body).to_not include(cep)}
   expect(@response.code).to eq 200
